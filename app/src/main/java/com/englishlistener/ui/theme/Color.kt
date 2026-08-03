ada72bac53a388f253fc82a64bf05c3b71c75f5f
zhco <luycheung@qq.com>
2026-08-03 18:55:58 +0800

    添加 app/src/main/java/com/englishlistener/ui/theme/Color.kt

---
+++ app/src/main/java/com/englishlistener/ui/theme/Color.kt
@@ -0,0 +1,31 @@
+package com.englishlistener.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// 品牌色
+val Primary = Color(0xFF1A73E8)
+val PrimaryVariant = Color(0xFF1557B0)
+val Secondary = Color(0xFF34A853)
+val Error = Color(0xFFEA4335)
+
+// 深色主题
+val DarkBackground = Color(0xFF121212)
+val DarkSurface = Color(0xFF1E1E1E)
+val DarkCard = Color(0xFF2C2C2C)
+
+// 浅色主题
+val LightBackground = Color(0xFFF8F9FA)
+val LightSurface = Color(0xFFFFFFFF)
+val LightCard = Color(0xFFFFFFFF)
+
+// 口音标签色
+val AccentBritish = Color(0xFFC0392B)
+val AccentAmerican = Color(0xFF2980B9)
+val AccentChinese = Color(0xFFD4AC0D)
+val AccentAustralian = Color(0xFF27AE60)
+val AccentOther = Color(0xFF7F8C8D)
+
+// 字幕区域
+val SubtitleBackground = Color(0xFF1A1A2E)
+val EnglishText = Color(0xFFE0E0E0)
+val ChineseText = Color(0xFFB0BEC5)
